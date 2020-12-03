5dc7a59af035e5bdf62ccd8f71cd7f30c60029d9
Mike <mgalvin@tribevr.io>
2020-12-03 17:41:51 -0600

    finished the MVP

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,8 +1,11 @@
+import axios from 'axios';
+console.log(axios);
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
+const profile = axios.get('https://api.github.com/users/mikefgalvin');
 
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
@@ -11,11 +14,56 @@
 
     Skip to STEP 3.
 */
+// console.log('profile', profile);
+
+const entryPoint = document.querySelector('.cards');
 
 /*
   STEP 4: Pass the data received from Github into your function,
     and append the returned markup to the DOM as a child of .cards
 */
+// axios.get('https://api.github.com/users/mikefgalvin')
+//   .then((res) => {
+//     console.log('data came thru', res);
+//     const profileContent = res.data;
+//     console.log('profile content', profileContent);
+//     const newProfileCard = profileMaker(profileContent);
+//     entryPoint.appendChild(newProfileCard);
+//   })
+//   .catch((error) => {
+//     console.log('it didnt work', error);
+//   })
+
+// followersArray.forEach((item) => { 
+//   const newProfile = profileMaker(item);
+//   entryPoint.appendChild(newProfile);
+// });
+
+const followersArray = [
+  'https://api.github.com/users/mikefgalvin',
+  'https://api.github.com/users/tetondan',
+  'https://api.github.com/users/dustinmyers',
+  'https://api.github.com/users/justsml',
+  'https://api.github.com/users/luishrd',
+  'https://api.github.com/users/bigknell',
+  ];
+
+followersArray.forEach((item) => { 
+  axios.get(item)
+  .then((res) => {
+    console.log('data came thru', res);
+    const profileContent = res.data;
+    console.log('profile content', profileContent);
+    const newProfileCard = profileMaker(profileContent);
+    entryPoint.appendChild(newProfileCard);
+  })
+  .catch((error) => {
+    console.log('it didnt work', error);
+  })
+})
+
+
+  
 
 /*
   STEP 5: Now that you have your own card getting added to the DOM, either
@@ -28,7 +76,27 @@
     user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+// 'https://api.github.com/users/mikefgalvin'
+// 'https://api.github.com/users/dustinmyers'
+// 'https://api.github.com/users/justsml'
+// 'https://api.github.com/users/luishrd'
+// 'https://api.github.com/users/bigknell'
+
+
+
+
+// const followerProfiles = followersArray.map((x) => {
+//   return makeProfiles;
+// });
+
+// makeProfiles.forEach((profile) => {
+//   entryPoint.appendChild(profile);
+// });
+
+// followersArray.forEach((item) => { 
+//   const newProfile = profileMaker(item);
+//   entryPoint.appendChild(newProfile);
+// });
 
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
@@ -49,6 +117,52 @@ const followersArray = [];
       </div>
     </div>
 */
+
+function profileMaker(profile) {
+
+  const card = document.createElement('div');
+  const image = document.createElement('img');
+  const cardInfo = document.createElement('div');
+  const name = document.createElement('h3');
+  const username = document.createElement('p');
+  const location = document.createElement('p');
+  const profileAddress = document.createElement('p');
+  const profileURL = document.createElement('a');
+  const followers = document.createElement('p');
+  const following = document.createElement('p');
+  const bio = document.createElement('p');
+
+  card.appendChild(image);
+  card.appendChild(cardInfo);
+  cardInfo.appendChild(name);
+  cardInfo.appendChild(username);
+  cardInfo.appendChild(location);
+  cardInfo.appendChild(profileAddress);
+  profileAddress.appendChild(profileURL);
+  cardInfo.appendChild(followers);
+  cardInfo.appendChild(following);
+  cardInfo.appendChild(bio);
+
+  card.classList.add('card');
+  cardInfo.classList.add('card-info');
+  name.classList.add('name');
+  username.classList.add('username');
+
+  image.src = profile.avatar_url; 
+  // console.log('image', image);
+  name.textContent = profile.name;
+  username.textContent = profile.login;
+  location.textContent = profile.location; 
+  profileAddress.textContent = profile.url;
+  following.textContent = profile.following;
+  followers.textContent = profile.followers;
+  bio.textContent = profile.bio;
+  profileURL.setAttribute('href', profile.html_url);
+
+    console.log('card', card);
+  // console.log('profile function', profile);
+  return card;
+}
 
 /*
   List of LS Instructors Github username's:
